c782a9dec9540dd20190a24df7027409d13b4503
Thenemex <tnmx777@gmail.com>
2025-07-20 22:11:45 +0200

    Set version to 1.0.0.3

--- src/main/java/nemexlib/NemexLib.java
+++ src/main/java/nemexlib/NemexLib.java
@@ -6,7 +6,7 @@ import static nemexlib.NemexLib.modID;
 import static nemexlib.NemexLib.dependencies;
 
 @SuppressWarnings("unused")
-@Mod(modid = modID, useMetadata = true, version = "1.0.0.2", dependencies = dependencies)
+@Mod(modid = modID, useMetadata = true, version = "1.0.0.3", dependencies = dependencies)
 public class NemexLib {
 
     public static final String modID = "NemexLib", dependencies = "required-after:Thaumcraft@[4.2.3.5,)";
