97818ea1f43314a665aed8da497581e02d491cba
Thenemex <tnmx777@gmail.com>
2025-10-12 14:38:16 +0200

    Added more useful constructors in Aspects class

--- src/main/java/nemexlib/api/thaumcraft/aspects/Aspects.java
+++ src/main/java/nemexlib/api/thaumcraft/aspects/Aspects.java
@@ -1,6 +1,7 @@
 package nemexlib.api.thaumcraft.aspects;
 
 import nemexlib.api.util.exceptions.ParameterArraysSizeException;
+import nemexlib.api.util.exceptions.ParameterIsNullOrEmpty;
 import nemexlib.api.util.exceptions.ParameterValueIsNegativeOrZero;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
@@ -16,10 +17,27 @@ public class Aspects extends AspectList {
      * @param aspect The aspect
      * @param amount The amount (1 = 1)
      * @throws ParameterValueIsNegativeOrZero If amount is zero or negative
+     * @throws ParameterIsNullOrEmpty If aspect is null
      */
     public Aspects(Aspect aspect, int amount) {
         if (amount <= 0) throw new ParameterValueIsNegativeOrZero(amount);
+        if (aspect == null) throw new ParameterIsNullOrEmpty();
         super.add(aspect, amount);
+    }
+
+    /**
+     * Constructor with multiple aspects and unique amount
+     * <p>Each aspect will have the same amount</p>
+     * @param aspects The aspect array
+     * @param amount The amount
+     * @throws ParameterValueIsNegativeOrZero If one of the amounts is zero or negative
+     * @throws ParameterIsNullOrEmpty If aspects is null or empty
+     */
+    public Aspects(int amount, Aspect ... aspects) {
+        if (amount <= 0) throw new ParameterValueIsNegativeOrZero(amount);
+        if (aspects == null || aspects.length == 0) throw new ParameterIsNullOrEmpty();
+        for (Aspect aspect : aspects)
+            super.add(aspect, amount);
     }
 
     /**
@@ -37,5 +55,18 @@ public class Aspects extends AspectList {
             if (amounts[i] <= 0) throw new ParameterValueIsNegativeOrZero(amounts[i]);
             super.add(aspects[i], amounts[i]);
         }
+    }
+
+    /**
+     * Constructor with multiple aspects
+     * <p>Each slot of the array will match the same slot from the other</p>
+     * <p>Exemple : <code>aspects[i]</code> will have <code>amounts[i]</code> as quantity</p>
+     * @param aspects The aspect array
+     * @param amounts The amount aspect
+     * @throws ParameterArraysSizeException If the two array sizes are different
+     * @throws ParameterValueIsNegativeOrZero If one of the amounts is zero or negative
+     */
+    public Aspects(int[] amounts, Aspect ... aspects) {
+        this(aspects, amounts);
     }
 }
