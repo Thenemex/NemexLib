cf3737571bf8d357ac582157a862dba3124fda8e
Thenemex <tnmx777@gmail.com>
2026-03-07 16:32:53 +0100

    Added full code for finding Crucible recipes Edited code of interface

---
+++ src/main/java/nemexlib/api/recipes/crucible/CrucibleFinder.java
@@ -0,0 +1,117 @@
+package nemexlib.api.recipes.crucible;
+
+import nemexlib.api.recipes.finders.ACollectionRecipeFinder;
+import net.minecraft.item.ItemStack;
+import thaumcraft.api.crafting.CrucibleRecipe;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@SuppressWarnings("rawtypes")
+public class CrucibleFinder extends ACollectionRecipeFinder<CrucibleRecipe> {
+
+    public CrucibleFinder(Collection ... registries) {
+        super(registries);
+    }
+    public CrucibleFinder(int amount, Collection ... registries) {
+        super(amount, registries);
+    }
+
+    @Override
+    public CrucibleRecipe findRecipeItem(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem());
+                    if (condition) return r;
+                } catch (Exception ignored) {}
+        return null;
+    }
+    @Override
+    public CrucibleRecipe findRecipeAmount(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem())
+                            && r.getRecipeOutput().stackSize == output.stackSize;
+                    if (condition) return r;
+                } catch (Exception ignored) {}
+        return null;
+    }
+    @Override
+    public CrucibleRecipe findRecipeMeta(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem())
+                            && r.getRecipeOutput().getItemDamage() == output.getItemDamage();
+                    if (condition) return r;
+                } catch (Exception ignored) {}
+        return null;
+    }
+    @Override
+    public CrucibleRecipe findRecipePrecise(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem())
+                            && r.getRecipeOutput().stackSize == output.stackSize
+                            && r.getRecipeOutput().getItemDamage() == output.getItemDamage();
+                    if (condition) return r;
+                } catch (Exception ignored) {}
+        return null;
+    }
+
+    @Override
+    public ArrayList<CrucibleRecipe> findRecipesItem(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem());
+                    if (condition) this.recipesFound.add(r);
+                } catch (Exception ignored) {}
+        return recipesFound;
+    }
+    @Override
+    public ArrayList<CrucibleRecipe> findRecipesAmount(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem())
+                            && r.getRecipeOutput().stackSize == output.stackSize;
+                    if (condition) this.recipesFound.add(r);
+                } catch (Exception ignored) {}
+        return recipesFound;
+    }
+    @Override
+    public ArrayList<CrucibleRecipe> findRecipesMeta(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem())
+                            && r.getRecipeOutput().getItemDamage() == output.getItemDamage();
+                    if (condition) this.recipesFound.add(r);
+                } catch (Exception ignored) {}
+        return recipesFound;
+    }
+    @Override
+    public ArrayList<CrucibleRecipe> findRecipesPrecise(ItemStack output) {
+        for (Collection registry : registries)
+            for (Object recipe : registry)
+                try {
+                    CrucibleRecipe r = (CrucibleRecipe) recipe;
+                    boolean condition = r.getRecipeOutput().getItem().equals(output.getItem())
+                            && r.getRecipeOutput().stackSize == output.stackSize
+                            && r.getRecipeOutput().getItemDamage() == output.getItemDamage();
+                    if (condition) this.recipesFound.add(r);
+                } catch (Exception ignored) {}
+        return recipesFound;
+    }
+}
--- src/main/java/nemexlib/api/recipes/finders/IRecipeFinder.java
+++ src/main/java/nemexlib/api/recipes/finders/IRecipeFinder.java
@@ -6,15 +6,15 @@ import java.util.ArrayList;
 
 public interface IRecipeFinder<TRecipe> {
 
-    void checkParameters(ItemStack item);
+    void checkParameters(ItemStack output);
 
-    TRecipe findRecipeItem(ItemStack item, boolean isOutput);
-    TRecipe findRecipeAmount(ItemStack item, boolean isOutput);
-    TRecipe findRecipeMeta(ItemStack item, boolean isOutput);
-    TRecipe findRecipePrecise(ItemStack item, boolean isOutput);
+    TRecipe findRecipeItem(ItemStack output);
+    TRecipe findRecipeAmount(ItemStack output);
+    TRecipe findRecipeMeta(ItemStack output);
+    TRecipe findRecipePrecise(ItemStack output);
 
-    ArrayList<TRecipe> findRecipesItem(ItemStack item, boolean isOutput);
-    ArrayList<TRecipe> findRecipesAmount(ItemStack item, boolean isOutput);
-    ArrayList<TRecipe> findRecipesMeta(ItemStack item, boolean isOutput);
-    ArrayList<TRecipe> findRecipesPrecise(ItemStack item, boolean isOutput);
+    ArrayList<TRecipe> findRecipesItem(ItemStack output);
+    ArrayList<TRecipe> findRecipesAmount(ItemStack output);
+    ArrayList<TRecipe> findRecipesMeta(ItemStack output);
+    ArrayList<TRecipe> findRecipesPrecise(ItemStack output);
 }
