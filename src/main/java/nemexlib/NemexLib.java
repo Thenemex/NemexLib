90e15fea5e4a8014f1e06e640286f69569a10c2c
Thenemex <tnmx777@gmail.com>
2026-03-08 02:31:07 +0100

    Changed version to 1.5

--- src/main/java/nemexlib/NemexLib.java
+++ src/main/java/nemexlib/NemexLib.java
@@ -1,13 +1,20 @@
 package nemexlib;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import nemexlib.api.commands.FindResearchFromRecipeCommand;
 
 import static nemexlib.NemexLib.modID;
 import static nemexlib.NemexLib.dependencies;
 
 @SuppressWarnings("unused")
-@Mod(modid = modID, useMetadata = true, version = "1.4", dependencies = dependencies)
+@Mod(modid = modID, useMetadata = true, version = "1.5", dependencies = dependencies)
 public class NemexLib {
 
     public static final String modID = "NemexLib", dependencies = "required-after:Thaumcraft@[4.2.3.5,)";
+
+    @Mod.EventHandler
+    public void onServerStarting(FMLServerStartingEvent event) {
+        event.registerServerCommand(new FindResearchFromRecipeCommand());
+    }
 }
