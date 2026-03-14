a4637eef4c54749db0621597088a552d8d54488b
Thenemex <tnmx777@gmail.com>
2026-03-15 00:01:04 +0100

    "Safed" the methods manipulating arrays in Util

--- src/main/java/nemexlib/api/util/Util.java
+++ src/main/java/nemexlib/api/util/Util.java
@@ -28,25 +28,16 @@ public class Util {
 
     public static String[] deepCopyTabAndAdd(String[] tab, String ... newElements) {
         if (tab == null || newElements == null || newElements.length == 0) throw new ParameterIsNullOrEmpty();
-        String[] deepCopy = new String[tab.length + newElements.length];
-        System.arraycopy(tab, 0, deepCopy, 0, tab.length);
-        System.arraycopy(newElements, 0, deepCopy, deepCopy.length - newElements.length - 1, newElements.length);
-        return deepCopy;
+        List<String> list = Arrays.asList(tab);
+        list.addAll(Arrays.asList(newElements));
+        return list.toArray(new String[0]);
     }
-    // ToDo Redo all code safe with ArrayList methods
 
     public static ResearchPage[] removeIndex(int index, ResearchPage[] array) {
         if (array == null || array.length == 0) throw new ParameterIsNullOrEmpty();
         if (index < 0 || index >= array.length) throw new IndexOutOfBoundsException(index, array.length);
-        ResearchPage[] newArray = new ResearchPage[array.length - 1];
-        boolean indexReached = false;
-        for (int i = 0; i < array.length; i++)
-            if (i != index && !indexReached) // Is not the element to remove AND element not found yet
-                newArray[i] = array[i];
-            else if (indexReached) // Element has been found in past iteration
-                newArray[i - 1] = array[i];
-            else // Element is found at current iteration
-                indexReached = true;
-        return newArray;
+        List<ResearchPage> list = Arrays.asList(array);
+        list.remove(index);
+        return list.toArray(new ResearchPage[0]);
     }
 }
