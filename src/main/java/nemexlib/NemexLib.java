e8257be702e9a680ba5c2164e8a060c42b06c920
Thenemex <tnmx777@gmail.com>
2026-03-14 21:30:43 +0100

    Set mod version to 1.8

--- src/main/java/nemexlib/NemexLib.java
+++ src/main/java/nemexlib/NemexLib.java
@@ -3,15 +3,18 @@ package nemexlib;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import nemexlib.api.commands.FindResearchFromRecipeCommand;
+import nemexlib.api.util.Logger;
 
 import static nemexlib.NemexLib.modID;
 import static nemexlib.NemexLib.dependencies;
 
 @SuppressWarnings("unused")
-@Mod(modid = modID, useMetadata = true, version = "1.7.1", dependencies = dependencies)
+@Mod(modid = modID, useMetadata = true, version = "1.8", dependencies = dependencies)
 public class NemexLib {
 
     public static final String modID = "NemexLib", dependencies = "required-after:Thaumcraft@[4.2.3.5,)";
+
+    public static final Logger logger = new Logger(modID);
 
     @Mod.EventHandler
     public void onServerStarting(FMLServerStartingEvent event) {
