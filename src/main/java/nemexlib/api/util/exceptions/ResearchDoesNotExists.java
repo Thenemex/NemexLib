65c9903becae693a07b4ed5b01d8c12240d2a74d
Thenemex <tnmx777@gmail.com>
2026-05-11 18:46:00 +0200

    Added new constructor in ResearchDoesNotExists, for research key/tag only

--- src/main/java/nemexlib/api/util/exceptions/ResearchDoesNotExists.java
+++ src/main/java/nemexlib/api/util/exceptions/ResearchDoesNotExists.java
@@ -2,6 +2,9 @@ package nemexlib.api.util.exceptions;
 
 public class ResearchDoesNotExists extends TCRException {
 
+    public ResearchDoesNotExists(String tag) {
+        super("The research with tag \"" + tag + "\" cannot be found.");
+    }
     public ResearchDoesNotExists(String tab, String tag) {
         super("The research with tag \"" + tag + "\", in tab \"" + tab + "\" cannot be found.");
     }
