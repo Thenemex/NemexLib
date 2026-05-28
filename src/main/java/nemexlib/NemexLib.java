b7c7da3016fed4681b13e92784a0401ed77c03de
Thenemex <tnmx777@gmail.com>
2026-05-28 18:10:00 +0200

    Set dev version to 1.11

--- src/main/java/nemexlib/NemexLib.java
+++ src/main/java/nemexlib/NemexLib.java
@@ -13,7 +13,7 @@ import static nemexlib.NemexLib.modID;
 import static nemexlib.NemexLib.dependencies;
 
 @SuppressWarnings("unused")
-@Mod(modid = modID, useMetadata = true, version = "1.10", dependencies = dependencies)
+@Mod(modid = modID, useMetadata = true, version = "1.11", dependencies = dependencies)
 public class NemexLib {
 
     public static final String modID = "NemexLib", dependencies = "required-after:Thaumcraft@[4.2.3.5,)";
