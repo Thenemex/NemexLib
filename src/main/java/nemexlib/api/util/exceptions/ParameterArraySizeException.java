3fbe078c010999ead52703bca8690c02a1a172e9
Thenemex <tnmx777@gmail.com>
2026-08-22 17:47:42 +0200

    Added one constructor in ParameterArraySizeException

--- src/main/java/nemexlib/api/util/exceptions/ParameterArraySizeException.java
+++ src/main/java/nemexlib/api/util/exceptions/ParameterArraySizeException.java
@@ -2,7 +2,11 @@ package nemexlib.api.util.exceptions;
 
 public class ParameterArraySizeException extends TCRException {
 
-    public ParameterArraySizeException(int size, int supposedSize){
+    public ParameterArraySizeException(int size, int supposedSize) {
         super("Parameter array is " + size + "but should be " + supposedSize);
+    }
+
+    public ParameterArraySizeException(int maxSize) {
+        super("Parameter array max size is " + maxSize);
     }
 }
